122e69fee0d50bda4525db5e929d6338b97fbea3
Kim Dong Min <kkimniu@gmail.com>
2025-06-17 15:26:03 +0900

    fix: 오타 수정 및 예외처리 IllegalArgumentException로 수정.

--- src/main/java/io/cavia/trader/module/notice/service/NoticeService.java
+++
@@ -1,8 +0,0 @@
-package io.cavia.trader.module.notice.service;
-
-import io.cavia.trader.module.notice.dto.NoticeDto;
-
-public interface NoticeService {
-
-    public int saveNotice(NoticeDto dto);
-}
--- src/main/java/io/cavia/trader/module/notice/service/NoticeServiceImple.java
+++ src/main/java/io/cavia/trader/module/notice/service/NoticeServiceImple.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Service;
 
 /**
  * 공지사항 서비스계층
+ *
  * @RequiredArgsConstructor는 자동으로 생성자를 만들기 위해서 사용함
  * 생성자주입을 사용한 이유는 @Autowired보다 테스트에 용이하다고 해서 사용함
  * mybatis에서는 서비스를 int로해서 컨트롤러에서 예외처리하는 것이 많다고함
  */
 @Service
 @RequiredArgsConstructor
-public class NoticeServiceImple implements NoticeService{
+public class NoticeService {
     private final NoticeMapper noticeMapper;
 
-    @Override
     public int saveNotice(NoticeDto dto) {
         int result = noticeMapper.saveNotice(dto);
         return result;
