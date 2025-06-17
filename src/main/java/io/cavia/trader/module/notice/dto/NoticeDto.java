52a114fe801ca317276a830b0c77d32357176568
Kim Dong Min <kkimniu@gmail.com>
2025-06-17 15:46:11 +0900

    fix: createdAt 카멜케이스에 맞게 수정.

--- src/main/java/io/cavia/trader/module/notice/dto/NoticeDto.java
+++ src/main/java/io/cavia/trader/module/notice/dto/NoticeDto.java
@@ -1,5 +1,6 @@
 package io.cavia.trader.module.notice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -9,15 +10,16 @@ import java.sql.Timestamp;
  * @AllArgsConstructor
  * @Getter
  * @Setter
- * @ToString
- * 를 대처하는 @Data를 사용함
+ * @ToString 를 대처하는 @Data를 사용함
  */
 @Data
 public class NoticeDto {
     private int id;
     private String title;
     private String content;
-    private Timestamp created_at;
-    private Timestamp updated_at;
+    @JsonProperty("created_at")
+    private Timestamp createdAt;
+    @JsonProperty("updated_at")
+    private Timestamp updatedAt;
     private boolean pinned;
 }
