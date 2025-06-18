83bf928682c49368a1a0cf5fcb9e2ef79578cfc5
Kim Dong Min <kkimniu@gmail.com>
2025-06-18 11:01:53 +0900

    feat: 공지사항 삭제/수정/조회/존재확인 Mapper 추가.

--- src/main/java/io/cavia/trader/module/notice/mapper/NoticeMapper.java
+++ src/main/java/io/cavia/trader/module/notice/mapper/NoticeMapper.java
@@ -3,6 +3,8 @@ package io.cavia.trader.module.notice.mapper;
 import io.cavia.trader.module.notice.dto.NoticeDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * 공지사항의 Mapper계층
  * mybaties랑 연결하기 위해서 사용함
@@ -10,4 +12,8 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface NoticeMapper {
     public int saveNotice(NoticeDto dto);
+    public int existsById(int id);
+    public List<NoticeDto> noticeListAll();
+    public int deleteNotice(int id);
+    public int updateNotice(NoticeDto dto);
 }
