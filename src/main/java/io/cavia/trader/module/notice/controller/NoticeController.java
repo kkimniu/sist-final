d48f8639e0c2dc7064998e75218953ea4b56739a
Kim Dong Min <kkimniu@gmail.com>
2025-06-18 11:12:12 +0900

    feat: 삭제 기능 추가.

--- src/main/java/io/cavia/trader/module/notice/controller/NoticeController.java
+++ src/main/java/io/cavia/trader/module/notice/controller/NoticeController.java
@@ -1,14 +1,13 @@
 package io.cavia.trader.module.notice.controller;
 
 import io.cavia.trader.module.notice.dto.NoticeDto;
-import io.cavia.trader.module.notice.exception.NoticeSaveFailedException;
+import io.cavia.trader.module.notice.exception.InvalidNoticeRequestException;
+import io.cavia.trader.module.notice.exception.NotFoundException;
+import io.cavia.trader.module.notice.exception.NoticeOperationFailedException;
 import io.cavia.trader.module.notice.service.NoticeServiceImple;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
@@ -29,16 +28,35 @@ public class NoticeController {
     @PostMapping("saveNotice")
     public ResponseEntity<String> saveNotice(@RequestBody NoticeDto noticeDto) {
         if (noticeDto.getTitle() == null || noticeDto.getTitle().isBlank()) {
-            throw new IllegalArgumentException("제목은 비워둘 수 없습니다");
+            throw new InvalidNoticeRequestException("제목은 비워둘 수 없습니다");
         }
         if (noticeDto.getContent() == null || noticeDto.getContent().isBlank()) {
-            throw new IllegalArgumentException("내용은 비워둘 수 없습니다");
+            throw new InvalidNoticeRequestException("내용은 비워둘 수 없습니다");
         }
         noticeDto.setCreatedAt(new Timestamp(System.currentTimeMillis()));
         int result = noticeServiceImple.saveNotice(noticeDto);
         if (result != 1) {
-            throw new NoticeSaveFailedException("공지사항 저장이 실패했습니다");
+            throw new NoticeOperationFailedException("공지사항 저장이 실패했습니다");
         }
         return ResponseEntity.status(201).body("공지사항 등록완료");
     }
+
+    @DeleteMapping("deleteNotice")
+    public ResponseEntity<String> deleteNotice(@RequestBody NoticeDto noticeDto) {
+        if (noticeDto.getId() == 0) {
+            throw new InvalidNoticeRequestException("유효하지 않는 ID 입니다");
+        }
+        boolean exitsts = noticeServiceImple.existsById(noticeDto.getId());
+        if (!exitsts) {
+            throw new NotFoundException("해당 아이디는 존재하지않습니다.");
+        }
+        int result = noticeServiceImple.deleteNotice(noticeDto.getId());
+        if (result != 1) {
+            throw new NoticeOperationFailedException("공지사항 삭제를 실패했습니다.");
+        }
+        return ResponseEntity.status(201).body("공지사항 삭제완료");
+    }
+
+    //@GetMapping("noticListAll")
+
 }
