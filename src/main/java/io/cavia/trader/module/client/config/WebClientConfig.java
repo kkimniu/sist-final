a5770aacc5380c2e1c9b23c44ed1bf24d24654af
xoz0359 <k84890690@gmail.com>
2025-06-24 10:23:25 +0900

    feat: 대용량 문자열 수신을 위해 WebClient 디코딩 버퍼 확장

--- src/main/java/io/cavia/trader/module/client/config/WebClientConfig.java
+++ src/main/java/io/cavia/trader/module/client/config/WebClientConfig.java
@@ -4,6 +4,7 @@ import io.cavia.trader.module.client.connector.RestWebClientImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -16,7 +17,15 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
+        // 대용량 문자열을 디코딩 하기 위해 WebClient의 응답 버퍼 사이즈 10mb로 설정
         return webClientBuilder
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configur
+                                .defaultCodecs()
+                                .maxInMemorySize(10 * 1024 * 1024)
+                        )
+                        .build()
+                )
                 .baseUrl(baseUrl)
                 .build();
     }
