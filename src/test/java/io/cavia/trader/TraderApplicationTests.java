15273e3e2070c3d832ef8ade1ceedb0478e5eadf
xoz0359 <k84890690@gmail.com>
2025-06-24 10:11:04 +0900

    feat: 스케줄러 사용을 위한 어노테이션 추가

--- src/test/java/io/cavia/trader/TraderApplicationTests.java
+++ src/test/java/io/cavia/trader/TraderApplicationTests.java
@@ -1,13 +1,19 @@
 package io.cavia.trader;
 
+import io.cavia.trader.module.game.service.GameAdministrationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TraderApplicationTests {
 
+	@Autowired
+	private GameAdministrationService gameAdministrationService;
+
 	@Test
 	void contextLoads() {
+		gameAdministrationService.createGame();
 	}
 
 }
