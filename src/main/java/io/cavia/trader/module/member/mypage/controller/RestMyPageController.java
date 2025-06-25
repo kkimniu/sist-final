2f209ec5a5f43de0b4e7d368d358c01ea89d739d
Kim Dong Min <kkimniu@gmail.com>
2025-06-25 12:51:50 +0900

    feat: 닉네임 수정과 닉네임 중복 RESTController 추가.

--- src/main/java/io/cavia/trader/module/member/mypage/controller/RestMyPageController.java
+++ src/main/java/io/cavia/trader/module/member/mypage/controller/RestMyPageController.java
@@ -2,14 +2,14 @@ package io.cavia.trader.module.member.mypage.controller;
 
 import io.cavia.trader.module.member.entity.Member;
 import io.cavia.trader.module.member.mypage.dto.GameParticipationDto;
+import io.cavia.trader.module.member.mypage.dto.NicknameUpdateRequestDto;
 import io.cavia.trader.module.member.mypage.service.MyPageService;
+import io.cavia.trader.module.member.service.SignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,14 +18,27 @@ import java.util.List;
 public class RestMyPageController {
 
     private final MyPageService mypageService;
+    private final SignupService signupService;
 
     @GetMapping("/member-id/{memberId}")
-    public ResponseEntity<List<GameParticipationDto>> getGameParticipationsByMemberId(@PathVariable int memberId){
+    public ResponseEntity<List<GameParticipationDto>> getGameParticipationsByMemberId(@PathVariable int memberId) {
         return ResponseEntity.status(200).body(mypageService.findByMemberId(memberId));
     }
-    @GetMapping("/id/{id}")
-    public ResponseEntity<Member> getMembersByEmail(@PathVariable Long id){
-        return ResponseEntity.status(200).body(mypageService.findById(id));
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Member> getMembersByEmail(@PathVariable Long id) {
+        return ResponseEntity.status(200).body(mypageService.findById(id));
+    }
+
+    @GetMapping("/check-nickname")
+    public ResponseEntity<String> getcheckNickname(@RequestParam String nickname){
+        signupService.validateDuplicateNickname(nickname);
+        return  ResponseEntity.status(200).body("중복없음");
+    }
+
+    @PatchMapping("/nickname")
+    public ResponseEntity<String> updateNickname(@RequestBody NicknameUpdateRequestDto nicknameUpdateRequestDto) {
+        mypageService.changeNickname(nicknameUpdateRequestDto.getId(),nicknameUpdateRequestDto.getNickname(),LocalDateTime.now());
+        return  ResponseEntity.status(200).body("변경성공");
     }
 }
