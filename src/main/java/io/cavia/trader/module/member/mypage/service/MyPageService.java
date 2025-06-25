32463bf63c0d9eea851135a4595687176a01780a
Kim Dong Min <kkimniu@gmail.com>
2025-06-25 17:29:21 +0900

    feat: 자산초기화시 초기 자산으로 변경 Service 추가.

--- src/main/java/io/cavia/trader/module/member/mypage/service/MyPageService.java
+++ src/main/java/io/cavia/trader/module/member/mypage/service/MyPageService.java
@@ -10,7 +10,8 @@ public interface MyPageService {
 
     public List<GameParticipationDto> findByMemberId(int memberId);
     public Member findById(Long id);
-    public void changeNickname(Long id, String nickname, LocalDateTime nicknameUpdatedAt);
+    public void changeNickname(int id, String nickname, LocalDateTime nicknameUpdatedAt);
     public boolean validateDuplicatePassword(int id,String password);
     public int changePassword(int id, String password, LocalDateTime passwordUpdatedAt);
+    public int resetCash(int id);
 }
