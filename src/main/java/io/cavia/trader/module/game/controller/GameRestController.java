b41b2ce9f5c7d6137637a133ac0a106206c343e6
xoz0359 <k84890690@gmail.com>
2025-06-27 10:20:24 +0900

    feat: 매퍼 추가, 파일 이름 변경

--- src/main/java/io/cavia/trader/module/game/controller/GameRestController.java
+++ src/main/java/io/cavia/trader/module/game/controller/GameRestController.java
@@ -13,12 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/game/api")
 @RequiredArgsConstructor
 public class GameRestApi {
+    /**
+     * 사용자가 게임 페이지에서 보내는 요청들을 처리하는 RestApi입니다
+     * validateAccess(String authorizationHeader) 사용자가 토큰을 포함해 요청을 보내면 인증 후 개인 정보가 담긴 Cliaims 객체를 반환합니다.
+     */
 
     private final JwtUtil jwtUtil;
 
     @PostMapping("/verify")
-    public ResponseEntity<?> validateAccess(@RequestHeader("Authorization") String authorization) {
-        String token = jwtUtil.substringToken(authorization);
+    public ResponseEntity<?> validateAccess(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = jwtUtil.substringToken(authorizationHeader);
         if(jwtUtil.validateToken(token)){
             Claims userInfo = jwtUtil.getUserInfoFromToken(token);
             return new ResponseEntity<Claims>(userInfo, HttpStatus.OK);
