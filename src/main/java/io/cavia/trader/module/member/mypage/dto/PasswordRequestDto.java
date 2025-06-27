71f9be4c95525b95d0be0d1e260c1560eba61385
Kim Dong Min <kkimniu@gmail.com>
2025-06-27 09:53:06 +0900

    feat: 패스워드 관련 DTO 추가.

---
+++ src/main/java/io/cavia/trader/module/member/mypage/dto/PasswordRequestDto.java
@@ -0,0 +1,12 @@
+package io.cavia.trader.module.member.mypage.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordRequestDto {
+    private String password;
+}
