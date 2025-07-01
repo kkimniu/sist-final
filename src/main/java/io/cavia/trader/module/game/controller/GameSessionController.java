e5dc212bd1516cca1435b7a288e08371a78219cd
xoz0359 <k84890690@gmail.com>
2025-07-01 16:57:26 +0900

    refactor: repository 추가, 안 쓰는 안터티 삭제

--- src/main/java/io/cavia/trader/module/game/controller/GameSessionController.java
+++ src/main/java/io/cavia/trader/module/game/controller/GameSessionController.java
@@ -2,7 +2,6 @@ package io.cavia.trader.module.game.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GameSessionController {
 
     @RequestMapping("")
-    public String testDetailPage() {
+    public String getGameMain() {
         return "game/main";
     }
 }
